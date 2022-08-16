286b42cddcfc74cd8c48799094cc42cfb88b7a1d
Michael Hodgkins <michaelhodgkins@Michaels-MacBook-Pro.local>
2022-08-16 19:07:41 +0100

    all done

--- models/User.model.js
+++ models/User.model.js
@@ -3,10 +3,10 @@ const { Schema, model } = require("mongoose");
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
 const userSchema = new Schema({
   username: {
-    type: String,
-    unique: true
+    type: String, trim: true, unique: true, required: true
   },
-  password: String
+  passwordHash: { type: String, required: true }
+  
 });
 
 const User = model("User", userSchema);
